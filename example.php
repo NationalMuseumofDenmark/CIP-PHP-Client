6ba50ccafe3cc016efa60e56137f8c76e1c4ee6f
Kræn Hansen (BIT BLUEPRINT) for the National Museum of Denmark <kh@bitblueprint.com>
2014-04-15 14:28:23 +0200

    Adding the example.php

---
+++ example.php
@@ -0,0 +1,10 @@
+<?php
+// Include and bootstrap the client.
+require("./src/CIP/CIPClient.php");
+// Instantiate the CIP client.
+$client = new \CIP\CIPClient('http://samlinger.natmus.dk/', false);
+// Request the system version.
+$response = $client->system()->getversion();
+// Print the version returned from the service.
+print_r($response['version']);
+?>
